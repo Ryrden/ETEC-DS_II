48c655c62d5e62046d5130513d6b09930950eb4d
Ryan <ryansateles@gmail.com>
2021-09-14 18:03:01 -0300

    Atividades Etec DS_2
    Atividades na root, o resto é configuração do usbwebserver

---
+++ usbwebserver/root/Agenda2/Book_study/Variaveis.php
@@ -0,0 +1,13 @@
+<?php
+    $nomeCompleto = "Ryan Souza Sá Teles";
+    $idade = 19;
+    $profissao = "Estudante";
+    $salario = 0.00;
+
+    echo  "Nome Completo: $nomeCompleto<br>"; 
+    echo  "Idade: $idade<br>"; 
+    echo  "Profissão: ". $profissao."<br>"; 
+    echo  "Salário: R$ ". $salario."<br>"; 
+
+
+?>
